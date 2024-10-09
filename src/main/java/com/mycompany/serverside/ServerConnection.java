bc322a5158a6c79df72721b47088fea1bccef52d
naqeebah014 <161496173+naqeebah014@users.noreply.github.com>
2024-10-09 12:44:27 +0200

    ff
    hh

---
+++ src/main/java/com/mycompany/serverside/ServerConnection.java
@@ -0,0 +1,59 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package com.mycompany.serverside;
+
+import java.net.*;
+import java.io.*;
+
+/**
+ *
+ * @author naqee
+ */
+public class ServerConnection {
+
+    private ServerSocket listener;
+    private ObjectInputStream in;
+    private ObjectOutputStream out;
+    private Socket client;
+    private String myMsg;
+
+    public ServerConnection() {
+        try {
+            listener = new ServerSocket(6666);
+        } catch (IOException e) {
+            System.out.println("Constructor ");
+            e.printStackTrace();
+        }
+
+    }
+
+    private void getStreams() throws ClassNotFoundException, IOException {
+        in = new ObjectInputStream(client.getInputStream());
+        out = new ObjectOutputStream(client.getOutputStream());
+    }
+
+    private void processClient() {
+    }
+
+    private void sendData(String myMsg) {
+        try {
+            out.writeObject(myMsg);
+
+        } catch (IOException e) {
+
+        }
+    }
+    private void listenForClients() throws ClassNotFoundException {
+        try {
+            client = listener.accept();
+            getStreams();
+            processClient();
+
+        } catch (IOException e) {
+
+        }
+
+    }
+}
